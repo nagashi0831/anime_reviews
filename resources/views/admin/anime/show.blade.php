dc7e0c8d2243f0de24315ec86993a7696ac8f3c8
”長島壮汰” <gmc_sou.custam@icloud.com>
2020-08-26 12:18:16 +0000

    コメント画面のレイアウト、文字色調整

--- resources/views/admin/anime/show.blade.php
+++ resources/views/admin/anime/show.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container mt-4">
+<div class="container mt-5">
     <a class="card-link" href="{{ action('Admin\AnimeController@index') }}">
         ↩アニメ一覧に戻る
     </a>
     <div class="border p-4">
+        <div class="show-message">
             <h1 class="h5 mb-4">
                 {{ $post->title }}
             </h1>
             
-            <p class="body-message mb-5">
+            <p class="mb-5">
                 {{ \Str::limit($post->body, 250) }}
             </p>
             {{--編集をクリック=>edit actionを呼び出す=>edit.blade.phpを呼び出して編集画面の表示--}}
@@ -19,52 +20,53 @@
                 <h2 class="h5 mb-4">
                     コメント
                 </h2>
-                <form class="mb-4" method="POST" action="{{ action('Admin\CommentsController@store') }}">
-                    @csrf
-                
-                    <input
-                        name="anime_id"
-                        type="hidden"
-                        value="{{ $post->id }}"
-                    >
-                
-                    <div class="form-group">
-                        <label for="body">
-                            本文
-                        </label>
-                
-                        <textarea
-                            id="body"
-                            name="body"
-                            class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"
-                            rows="4"
-                        >{{ old('body') }}</textarea>
-                        @if ($errors->has('body'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('body') }}
-                            </div>
-                        @endif
-                    </div>
-                
-                    <div class="mt-4">
-                        <button type="submit" class="btn btn-primary">
-                            コメントする
-                        </button>
-                    </div>
-                </form>
-                @forelse($post->comments as $comment)
-                <div class="border-top p-4">
-                    <time class="text-secondary">
-                        {{ $comment->created_at->format('Y.m.d H:i') }}
-                    </time>
-                    <p class="mt-2">
-                        {{ \Str::limit($comment->body, 100) }}
-                    </p>
+        </div>
+            <form class="mb-4" method="POST" action="{{ action('Admin\CommentsController@store') }}">
+                @csrf
+            
+                <input
+                    name="anime_id"
+                    type="hidden"
+                    value="{{ $post->id }}"
+                >
+            
+                <div class="form-group">
+                    <label for="body">
+                        本文
+                    </label>
+            
+                    <textarea
+                        id="body"
+                        name="body"
+                        class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"
+                        rows="4"
+                    >{{ old('body') }}</textarea>
+                    @if ($errors->has('body'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('body') }}
+                        </div>
+                    @endif
                 </div>
-                @empty
-                <p>コメントはまだありません</p>
-                @endforelse
-            </section>
+            
+                <div class="mt-4">
+                    <button type="submit" class="btn btn-primary">
+                        コメントする
+                    </button>
+                </div>
+            </form>
+            @forelse($post->comments as $comment)
+            <div class="border-top p-4">
+                <time class="text-secondary">
+                    {{ $comment->created_at->format('Y.m.d H:i') }}
+                </time>
+                <p class="mt-2">
+                    {{ \Str::limit($comment->body, 100) }}
+                </p>
+            </div>
+            @empty
+            <p>コメントはまだありません</p>
+            @endforelse
+        </section>
     </div>
 </div>
 @endsection
